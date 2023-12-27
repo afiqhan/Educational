0135f09d09f86e30a6ff0dc59ce57a256d78003c
afiqqo <afiqhanif12345@gmail.com>
2023-12-27 22:14:41 +0800

    Lab 7 already done

---
+++ Native Mobile Programming/Lab7/Task1/app/src/main/java/com/alomory/lab7/MainActivity.java
@@ -0,0 +1,41 @@
+package com.alomory.lab7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    SensorManager senseMan;
+
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = (ListView)findViewById(R.id.listview);
+
+        senseMan = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        List<Sensor> sensorList = senseMan.getSensorList(Sensor.TYPE_ALL);
+
+        // Extract sensor names from the list
+        String[] sensorNames = new String[sensorList.size()];
+        for (int i = 0; i < sensorList.size(); i++) {
+            sensorNames[i] = sensorList.get(i).getName();
+        }
+
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sensorNames));
+        System.out.println(Arrays.asList(sensorList));
+    }
+}
---
+++ Native Mobile Programming/Lab7/Task2/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Task 2"
+include(":app")
+ 
