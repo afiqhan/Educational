b3763418dbb9a3e644ca98f99f4c6f6d7be5fb56
afiqqo <afiqhanif12345@gmail.com>
2024-01-08 02:23:27 +0800

    Native

---
+++ Native Mobile Programming/Lab2/app/src/main/java/com/afiq/happybirthday/MainActivity.kt
@@ -0,0 +1,197 @@
+package com.afiq.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Build
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.alomory.happybirthday.ui.theme.HappyBirthDayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthDayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   ProfileBuilderApp()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun ProfileBuilderApp() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+            .background(Color(0Xffd2e8d4)),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+
+            modifier = Modifier
+                .padding(16.dp)
+                .weight(1f)
+                .fillMaxHeight(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom,
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                Box(
+
+                    modifier = Modifier
+                        .size(100.dp)
+                        .background(Color(0xFF073042))
+                ) {
+
+                    Image(
+                        painter = painterResource(id = R.drawable.android_logo),
+                        contentDescription = null,
+                        modifier = Modifier.size(100.dp)
+                    )
+                }
+                Text(
+                    text = stringResource(R.string.developer_name_text),
+                    Modifier.fillMaxWidth(),
+                    fontSize = 40.sp,
+                    textAlign = TextAlign.Center,
+
+                )
+                Text(
+                    text = stringResource(R.string.developer_desc_text),
+                    Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xff006d3a)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .weight(1f),
+            horizontalArrangement = Arrangement.Center,
+
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Bottom,
+                modifier = Modifier.fillMaxSize()
+
+            ) {
+                Row (
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    Icon(
+                        Icons.Rounded.Phone,
+                        contentDescription = null,
+                        Modifier.padding(start = 80.dp, end = 25.dp, bottom = 20.dp),
+                           tint = Color(0xff006d3a)
+                    )
+
+                    Text(text = stringResource(R.string.phone_no_text))
+                }
+                Row (
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(Icons.Rounded.Share,
+                        contentDescription = null,
+                        Modifier.padding(start = 80.dp, end = 25.dp, bottom = 20.dp),
+                        tint = Color(0xff006d3a)
+                    )
+                    Text(text = stringResource(R.string.domain_text))
+                }
+                Row (
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ){
+                    Icon(Icons.Rounded.Email,
+                        contentDescription = null,
+                        Modifier.padding(start = 80.dp, end = 25.dp, bottom = 25.dp),
+                        tint = Color(0xff006d3a)
+                    )
+                    Text(text = stringResource(R.string.email_text))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun ComposeArticle() {
+    HappyBirthDayTheme {
+        ProfileBuilderApp()
+    }
+}
